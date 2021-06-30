466b88332984cde47fd891b9091d1a14917817ce
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-06-30 11:05:04 -0300

    Correção do CRUD do Produtos

--- App/Controllers/Produtos.php
+++ App/Controllers/Produtos.php
@@ -2,49 +2,62 @@
 
 use App\Core\Controller;
 
-class Produtos extends Controller {
+class Produtos extends Controller
+{
 
     // lista todos os produtos
-    public function index() {
+    public function index()
+    {
         $produtoModel = $this->model("Produto");
 
-        $dados = $produtoModel->listarTodos();
+        $produtos = $produtoModel->listarTodos();
 
-       //colocar os dados no corpo da requisição
-       echo json_encode($dados, JSON_UNESCAPED_UNICODE);
+        $produtos = array_map(function ($p) {
+            $p->categoria = ["id" => $p->categoria_id, "descricao" => $p->categoria];
+            unset($p->categoria_id);
+            return $p;
+        }, $produtos);
+
+        //colocar os produtos no corpo da requisição
+        echo json_encode($produtos, JSON_UNESCAPED_UNICODE);
     }
 
-    public function find($id) {
+    public function find($id)
+    {
         $produtoModel = $this->model("Produto");
+
         $produtoModel = $produtoModel->buscarPorId($id);
 
-        if($produtoModel){
-           echo json_encode($produtoModel, JSON_UNESCAPED_UNICODE);
-        }else{
-            //não encontrou a produto id
-            http_response_code(404);
+        if ($produtoModel) {
+            $produtoModel->categoria = [
+                "id" => $produtoModel->categoria_id,
+                "descricao" => $produtoModel->categoria
+            ];
+            unset($produtoModel->categoria_id);
 
-            $erro = ["erro" => "Produto não encontrada"];
-            
-            echo json_encode($erro, JSON_UNESCAPED_UNICODE);
+            echo json_encode($produtoModel, JSON_UNESCAPED_UNICODE);
+        } else {
+            http_response_code(404);
+            json_encode(["erro" => "Produto não encontrado"]);
         }
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
         //instanciamos o model, colocando nele a descricao recebida
         $produtoModel = $this->model("produto");
         $produtoModel = $produtoModel->buscarPorId($id);
-  
+
         //verificando se o id passado não existe, retornando erro
-        if(!$produtoModel){
-              http_response_code(404);
-              echo json_encode(["erro" => "produto não encontrada"]);
-              exit;
+        if (!$produtoModel) {
+            http_response_code(404);
+            echo json_encode(["erro" => "produto não encontrada"]);
+            exit;
         }
- 
-        if($produtoModel->deletar()){
+
+        if ($produtoModel->deletar()) {
             http_response_code(204);
-        }else{
+        } else {
             http_response_code(500);
             echo json_encode(["erro" => "Problemas ao excluir a produto"]);
         }
--- App/Models/Produto.php
+++ App/Models/Produto.php
@@ -2,7 +2,8 @@
 
 use App\Core\Model;
 
-class Produto {
+class Produto
+{
 
     public $id;
     public $descricao;
@@ -14,58 +15,52 @@ class Produto {
     public $desconto;
     public $imagem;
 
-    public function listarTodos(){
+    public function listarTodos()
+    {
         $sql = " SELECT p.*, c.descricao as categoria FROM tbl_produto p
                  INNER JOIN tbl_categoria c ON p.categoria_id = c.id ORDER BY p.id DESC ";
 
         $stmt = Model::getConn()->prepare($sql);
         $stmt->execute();
 
-        if($stmt->rowCount() > 0){
+        if ($stmt->rowCount() > 0) {
             $resultado = $stmt->fetchAll(\PDO::FETCH_OBJ);
 
             return $resultado;
-        }else{
+        } else {
             return [];
         }
     }
+    public function buscarPorId($id)
+    {
+        $sql = " SELECT p.*, c.descricao as categoria FROM tbl_produto p
+        INNER JOIN tbl_categoria c ON p.categoria_id = c.id
+        WHERE p.id = ? ";
 
-    public function buscarPorId($id) {
-
-        $sql = " SELECT * FROM tbl_produto WHERE id = ? ";
+        //preparamos a consulta
         $stmt = Model::getConn()->prepare($sql);
         $stmt->bindValue(1, $id);
+        //executamos a consulta
+        $stmt->execute();
 
-        if ($stmt->execute()) {
+        //verificamos a quantidade de linhas
+        if ($stmt->rowCount() > 0) {
+            //pegamos os resultados em forma de lista de objetos
+            $resultado = $stmt->fetch(\PDO::FETCH_OBJ);
 
-            $produto = $stmt->fetch(PDO::FETCH_OBJ);
-            if (!$produto) {
-                return false;
-            }
-
-            $this->id = $produto->id;
-            $this->descricao = $produto->descricao;
-            $this->peso = $produto->peso;
-            $this->quantidade = $produto->quantidade;
-            $this->cor = $produto->cor;
-            $this->tamanho = $produto->tamanho;
-            $this->valor = $produto->valor;
-            $this->desconto = $produto->desconto;
-            $this->imagem = $produto->imagem;
-            $this->categoria_id = $produto->categoria_id;
-            return $this;
-
+            //retornamos o resultado
+            return $resultado;
         } else {
-            return false;
+            return null;
         }
     }
 
-    public function deletar() {
+    public function deletar()
+    {
 
         $sql = " DELETE FROM tbl_produto WHERE id = ? ";
         $stmt = Model::getConn()->prepare($sql);
         $stmt->bindValue(1, $this->id);
         return $stmt->execute();
     }
-
 }
